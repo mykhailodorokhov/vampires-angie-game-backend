af62e125df4391ce30e5993ca91d7ee8d7f446f0
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2024-10-11 05:05:37 +0300

    revert to manual server start

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,29 @@ const fastify = Fastify();
 
 fastify.register(routes);
 
-export default async (req: any, res: any) => {
-  await fastify.ready();
-  fastify.server.emit("request", req, res);
-};
+fastify.register(async () => {
+  await fastify.route({
+    method: "GET",
+    url: "/",
+    handler: async (request, reply) => {
+      return reply.code(200).send("Welcome to Angie's Vampire game backend!");
+    },
+  });
+});
+
+async function main() {
+  try {
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    console.log("server is running ✅");
+  } catch (e) {
+    console.log("error 🫡");
+    console.log((e as Error).message);
+  }
+}
+
+main();
+
+// export default async (req: any, res: any) => {
+//   await fastify.ready();
+//   fastify.server.emit("request", req, res);
+// };
